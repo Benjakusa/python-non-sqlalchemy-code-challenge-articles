f24346238d8600777c49facc517f8bb6a868a0bc
Benjakusa <benjakusa@gmail.com>
2026-01-15 13:15:47 +0300

    author functions

--- lib/author.py
+++ lib/author.py
@@ -1,19 +1,47 @@
 class Author:
+    all_authors = []
+    
     def __init__(self, name):
-        self.name = name
-        self._articles = []  
+        # Initialize author with name
+        self._name = name
+        # Add author to list of all authors
+        Author.all_authors.append(self)
     
     @property
     def name(self):
+        # Getter for name property
         return self._name
     
     @name.setter
     def name(self, value):
+        # Name cannot be changed after instantiation
+        # hasattr checks if attribute exists (was set in __init__)
+        if hasattr(self, '_name'):
+          return
+        # Validate name type and length
         if not isinstance(value, str):
-            raise ValueError("Name must be a string")
+            raise TypeError("Name must be a string")
         if len(value) <= 0:
             raise ValueError("Name must be longer than 0 characters")
-        # If name already exists (hasattr check), don't allow change
-        if hasattr(self, '_name'):
-            raise AttributeError("Name cannot be changed after instantiation")
         self._name = value
+    
+    def articles(self):
+        from article import Article
+        return [article for article in Article.all_articles if article.author == self]
+    
+    def magazines(self):
+        magazine_list = [article.magazine for article in self.articles()]
+        # Remove duplicates using set and return as list
+        return list(set(magazine_list))
+    
+    def add_article(self, magazine, title):
+        # Create new article linking this author to magazine
+        from article import Article
+        return Article(self, magazine, title)
+    
+    def topic_areas(self):
+        # Return unique list of magazine categories author has written for
+        if not self.articles():
+            return None
+        categories = [magazine.category for magazine in self.magazines()]
+        return list(set(categories))
