b2cbf14a90821f68970d5f28502cf437fd977bb6
Benjakusa <benjakusa@gmail.com>
2026-01-15 09:03:07 +0300

    name setter

--- lib/author.py
+++ lib/author.py
@@ -6,3 +6,13 @@ class Author:
     @property
     def name(self):
         return self._name
+     @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if len(value) <= 0:
+            raise ValueError("Name must be longer than 0 characters")
+        # If name already exists (hasattr check), don't allow change
+        if hasattr(self, '_name'):
+            raise AttributeError("Name cannot be changed after instantiation")
+        self._name = value
