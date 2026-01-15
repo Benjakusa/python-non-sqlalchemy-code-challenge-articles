7936faafff15abe5a527695805f204c9bd8e3517
Benjakusa <benjakusa@gmail.com>
2026-01-15 13:22:01 +0300

    article

--- lib/article.py
+++ lib/article.py
@@ -1,8 +1,11 @@
 class Article:
+    all_articles = []
+    
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all_articles.append(self)
     
     @property
     def title(self):
@@ -10,11 +13,32 @@ class Article:
     
     @title.setter
     def title(self, value):
+        if hasattr(self, '_title'):
+            return
         if not isinstance(value, str):
-            raise ValueError("Title must be a string")
+            raise TypeError("Title must be a string")
         if len(value) < 5 or len(value) > 50:
             raise ValueError("Title must be between 5 and 50 characters")
-        # If title already exists, don't allow change
-        if hasattr(self, '_title'):
-            raise AttributeError("Title cannot be changed after instantiation")
         self._title = value
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        from author import Author
+        if not isinstance(value, Author):
+            raise TypeError("Author must be an instance of Author class")
+        self._author = value
+    
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, value):
+        from magazine import Magazine
+        if not isinstance(value, Magazine):
+            raise TypeError("Magazine must be an instance of Magazine class")
+        self._magazine = value
